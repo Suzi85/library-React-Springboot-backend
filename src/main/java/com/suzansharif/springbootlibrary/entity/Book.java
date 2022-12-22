f80572b2088819d541196946e32c380a28497aca
Sharifulnasaby, Suzan <suzan.sharifulnasaby@cgi.com>
2022-12-22 12:07:19 -0500

    initial commit

---
+++ src/main/java/com/suzansharif/springbootlibrary/entity/Book.java
@@ -0,0 +1,6 @@
+package com.suzansharif.springbootlibrary.entity;
+import lombok.Data;
+import javax.persistence.*;
+
+public class Book {
+}
---
+++ src/test/java/com/suzansharif/springbootlibrary/SpringBootLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suzansharif.springbootlibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
