b9d60d8ea5e1b236e96cd97aa9cc35fc4a1eccd7
Sharifulnasaby, Suzan <suzan.sharifulnasaby@cgi.com>
2022-12-22 16:03:07 -0500

    fixed redirection to login page by removing Okta dependecy

--- src/main/java/com/suzansharif/springbootlibrary/dao/BookRepository.java
+++ src/main/java/com/suzansharif/springbootlibrary/dao/BookRepository.java
@@ -2,6 +2,7 @@ package com.suzansharif.springbootlibrary.dao;
 
 import com.suzansharif.springbootlibrary.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 
 public interface BookRepository extends JpaRepository<Book, Long> {
